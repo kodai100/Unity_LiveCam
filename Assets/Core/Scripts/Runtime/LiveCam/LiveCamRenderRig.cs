0bb203d97052777062759898bd37fb46edefa772
Kodai Takao (Fork HomeWorkstation) <kodai100@gmail.com>
2021-09-20 22:16:08 +0900

    use MULTI_DISPLAY script define symbol for multi-display output

--- Assets/Core/Scripts/Runtime/LiveCam/LiveCamRenderRig.cs
+++ Assets/Core/Scripts/Runtime/LiveCam/LiveCamRenderRig.cs
@@ -5,30 +5,48 @@ using System.IO;
 
 namespace kodai100.LiveCamCore
 {
+#if MULTI_DISPLAY
     [Serializable]
     public class DisplayInfo
     {
         public int display1;
         public int display2;
     }
+#endif
 
     public class LiveCamRenderRig : MonoBehaviour
     {
         [SerializeField] private RawImage editorImage;
         [SerializeField] private RawImage outImage;
 
-        private string JsonFilePath = Application.streamingAssetsPath + "/Display.json";
-
-        private bool multiDisplay;
-
-        private DisplayInfo info;
 
         private void Start()
         {
+#if MULTI_DISPLAY
             info = Read();
             Debug.Log($"Display {info.display1}, {info.display2}");
+#endif
         }
 
+        public void SetTexture(RenderTexture renderTexture)
+        {
+            editorImage.texture = renderTexture;
+            outImage.texture = renderTexture;
+        }
+
+        public void Update()
+        {
+#if MULTI_DISPLAY
+           MultiDisplayUpdate();
+#endif
+        }
+
+
+#if MULTI_DISPLAY
+        private string JsonFilePath = Application.streamingAssetsPath + "/Display.json";
+
+        private DisplayInfo info;
+        
         public DisplayInfo Read()
         {
             var jsonDeserializedData = new DisplayInfo();
@@ -49,27 +67,19 @@ namespace kodai100.LiveCamCore
 
             return jsonDeserializedData;
         }
-
-        public void SetTexture(RenderTexture renderTexture)
-        {
-            editorImage.texture = renderTexture;
-            outImage.texture = renderTexture;
-        }
-
-        public void Update()
+        
+        private void MultiDisplayUpdate()
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                if (!multiDisplay)
+                if (Display.displays.Length > 1)
                 {
-                    if (Display.displays.Length > 1)
-                    {
-                        Display.displays[info.display1].Activate();
-                        Display.displays[info.display2].SetParams(1280, 720, 0, 0);
-                        Screen.SetResolution(1280, 720, false);
-                    }
+                    Display.displays[info.display1].Activate();
+                    Display.displays[info.display2].SetParams(1280, 720, 0, 0);
+                    Screen.SetResolution(1280, 720, false);
                 }
             }
         }
+#endif
     }
 }
